539392b155cff1f75c4cd46cc2fb8d7fc313e0f4
pmhuizinga <pmhuizinga@hotmail.com>
2022-12-04 21:49:33 +0100

    day3

--- aoc_code_3.py
+++ aoc_code_3.py
@@ -19,3 +19,13 @@ for item in data:
 
 # day 3 A
 print(nr)
+
+
+chunks = [data[x:x+3] for x in range(0, len(data), 3)]
+
+nr = 0
+for chunk in chunks:
+    matching_char = list(set(chunk[0]).intersection(chunk[1]).intersection(chunk[2]))
+    nr += d[matching_char[0]]
+
+print(nr)
