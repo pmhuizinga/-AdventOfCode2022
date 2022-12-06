d4739d03f5bdcc41b88afa81da6de61b907af133
pmhuizinga <pmhuizinga@hotmail.com>
2022-12-06 20:53:20 +0100

    day4

--- aoc_code_4.py
+++ aoc_code_4.py
@@ -1,11 +1,30 @@
 from general_functions import read_data
-import numpy as np
 
 day = 4
-data = read_data(day, test=True)
+data = read_data(day, test=False)
 data = [x.split(',') for x in data]
-# [['2-4', '6-8'], ['2-3', '4-5'], ['5-7', '7-9'], ['2-8', '3-7'], ['6-6', '4-6'], ['2-6', '4-8']]
-[[[int(z) for z in x.split("-")] for x in y] for y in data]
-# [[[2, 4], [6, 8]], [[2, 3], [4, 5]], [[5, 7], [7, 9]], [[2, 8], [3, 7]], [[6, 6], [4, 6]], [[2, 6], [4, 8]]]
+d = [[[int(z) for z in x.split("-")] for x in y] for y in data]
 
-# [int(x) for x in a.split("-")]
+counter = 0
+
+for x in d:
+
+    max_val = max(x[0][0], x[1][0])
+    min_val = min(x[0][1], x[1][1])
+
+    a = [min(min_val, max_val), max(min_val, max_val)]
+    if x[0] == a or x[1] == a:
+        counter += 1
+
+# day 4 A
+print(counter)
+
+counter = 0
+for x in d:
+    a = range(int(x[0][0]), int(x[0][1])+1)
+    b = range(int(x[1][0]), int(x[1][1])+1)
+    if set(a).intersection(b):
+        counter += 1
+
+# day 4 B
+print(counter)
