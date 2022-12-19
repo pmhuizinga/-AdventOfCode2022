11548d41991f4569765fd6c3fb2932b8218993e5
pmhuizinga <pmhuizinga@hotmail.com>
2022-12-19 22:20:40 +0100

    notebook

--- aoc_code_5.py
+++ aoc_code_5.py
@@ -15,17 +15,25 @@ for idx, x in enumerate(data):
 nr_of_stacks = max(stack[-1])
 stack = np.array([[x.replace("[", " ").replace("]", " ") for x in y] for y in stack])
 
-for column in range(0, stack.shape[1], 1):
-    print(stack[:, column])
-
+# remove blanks
 df = pd.DataFrame(stack)
-
 nan_value = float("NaN")
 df.replace(" ", nan_value, inplace=True)
 df.dropna(how='all', axis=1, inplace=True)
-
+df = df[:-1] # remove bottom row
+df.columns = np.arange(1, df.shape[1]+1) # rename columns
 
 procedure = []
 for idx2, x in enumerate(data):
-    if idx2 > idx + 1:
+    if idx2 >= idx + 1:
         procedure.append(x)
+
+def proces(step):
+    input = step.split(" ")
+    nr_of_item = input[1]
+    source = input[3]
+    target = input[5]
+    print(f'source: {source}, target: {target}, item: {nr_of_item}')
+
+for step in procedure:
+    proces(step)
